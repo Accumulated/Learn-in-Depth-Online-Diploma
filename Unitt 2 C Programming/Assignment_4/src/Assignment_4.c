0afe3c24c7cd305ab11283476a19532c679f8774
Mohamed Abdelmotaleb <muhammedmahmoud063@gmail.com>
2021-09-29 16:11:31 +0200

    Add files via upload

---
+++ Unitt 2 C Programming/Assignment_4/src/Assignment_4.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : Assignment_4.c
+ Author      : Mohamed M. Abdelmotaleb
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Assignment 4
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Functions.h"
+
+int main(void)
+{
+	printf("\nExample 1: Prime Numbers Between two Intervals by Making User-defined Function\n");
+	EX1();
+	printf("\nExample 2: C program to Calculate Factorial ofa Number Using Recursion\n");
+	EX2();
+	printf("\nExample 3: C program to Reverse a Sentence Using Recursion\n");
+	EX3();
+	printf("\nExample 4: C program to Calculate the Power of a Number Using Recursion\n");
+	EX4();
+
+}
+
