068ca364f38e33a127a8793d21b55bf406bb1932
Abdelmotaleb <muhammedmahmoud063@gmail.com>
2022-12-17 16:17:32 +0200

    Update commons

--- Unit 3 Embedded C/HW1/common.h
+++ Unit 3 Embedded C/HW1/common.h
@@ -1,14 +1,21 @@
 #ifndef	CMM_H
 #define	CMM_H
 
+
+
 typedef unsigned char 		uint8_t;
 typedef signed char 		sint8_t;
+
 
 typedef signed int			sint32_t;
 typedef unsigned int		uint32_t;
 
+
 typedef unsigned short int	uint16_t;
 typedef signed short int	sint16_t;
+
+
+
 
 
 #endif
